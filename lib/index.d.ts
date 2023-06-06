a0abcc1e8ebbeec874bb049bc93f3a79f441ec83
Ryan Daley <rpdaley@gmail.com>
2023-06-06 15:19:35 -0400

    post install commit

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+/**
+ * Initialization data for the plotly-extension extension.
+ */
+declare const plugin: JupyterFrontEndPlugin<void>;
+export default plugin;
