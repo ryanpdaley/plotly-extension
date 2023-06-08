81cc64de67348973a83f09fed19af2acb7cc4cab
Ryan Daley <rpdaley@gmail.com>
2023-06-08 11:35:39 -0400

    added css and max length

--- src/plotly.tsx
+++ src/plotly.tsx
@@ -3,29 +3,49 @@ import { ReactWidget } from '@jupyterlab/apputils';
 import React, { useState } from 'react';
 
 const PlotlyDialog = (): JSX.Element => {
-  const defaultText = 'Please enter some text';
-  const [text, setText] = useState(defaultText);
+  const defaultText = 'Please enter some text (max 150 characters)';
+  const [text, setText] = useState('');
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
     let alertMsg = `You entered: '${text}'`;
-    if (text === defaultText) {
-      alertMsg = "Hmm... you didn't enter any text. Try entering some text!";
+    if (text === '') {
+      alertMsg = "Hmm... you didn't enter any text.\n\nTry entering some text!";
     }
 
     alert(alertMsg);
   };
 
+  const handleOnChange = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    const currentText = e.target.value;
+    if (currentText.length > 150) {
+      alert(
+        "Whoa - that's a lot of text. Keep it to less than 150 characters!"
+      );
+    } else {
+      setText(currentText);
+    }
+  };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>Plotly Extension:</label>
-        <input
-          type="text"
+    <div className="plotly-wrapper">
+      <div className="plotly-header">
+        <h1>Plotly Widget</h1>
+        <hr></hr>
+      </div>
+      <form className="plotly-form" onSubmit={handleSubmit}>
+        <textarea
           value={text}
-          onChange={e => setText(e.target.value)}
+          onChange={handleOnChange}
+          placeholder={defaultText}
+          rows={4}
+          className="plotly-textarea"
+          maxLength={151}
         />
-        <button type="submit">OK</button>
+        <button className="plotly-button-ok" type="submit">
+          OK
+        </button>
       </form>
     </div>
   );
